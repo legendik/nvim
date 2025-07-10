524f7382ec53e7e827794854d3acc8ad59a237c0
Marek Šneberger <marek@sneberger.cz>
2025-07-10 11:00:56 +0200

    feat: use prettier for most files and markdownlint for markdown

--- init.lua
+++ init.lua
@@ -669,7 +669,13 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        ts_ls = {
+          capabilities = vim.tbl_deep_extend('force', capabilities, {
+            textDocument = {
+              formatting = false,
+            },
+          }),
+        },
         --
 
         lua_ls = {
@@ -760,7 +766,15 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+        json = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
+        scss = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        markdown = { 'markdownlint', 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
